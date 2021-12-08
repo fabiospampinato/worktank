72c36af0f42e29a0fd5337b1dada686de45093b3
Fabio Spampinato <spampinabio@gmail.com>
2021-12-08 00:01:07 +0000

    Worker backend: ensuring the bundled code is properly escaped

--- tasks/compile_worker.sh
+++ tasks/compile_worker.sh
@@ -2,6 +2,7 @@
 # Precompiling the worker backend into a string, which can then be loaded more easily
 
 WORKER=`esbuild src/worker/backend.ts --bundle --minify --target=es2017 --external:"worker_threads"`
-BUNDLE="\n/* COMPILED WORKER */\n\nexport default '$WORKER';"
+WORKER_ESCAPED=`echo $WORKER | sed "s/'/\\\\\\'/g"`
+BUNDLE="\n/* COMPILED WORKER */\n\nexport default '$WORKER_ESCAPED';"
 
 echo $BUNDLE > src/worker/backend_compiled.ts
