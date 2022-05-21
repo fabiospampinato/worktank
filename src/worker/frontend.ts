9c259829113f2cdf5fc6b74234887e5722d2e874
Fabio Spampinato <spampinabio@gmail.com>
2022-05-22 00:20:13 +0100

    Replaced web-worker with webworker-shim, which is much more bundler friendly

--- src/worker/frontend.ts
+++ src/worker/frontend.ts
@@ -1,7 +1,7 @@
 
 /* IMPORT */
 
-import WorkerShim from 'web-worker';
+import WorkerShim from 'webworker-shim';
 import WorkerBackend from './backend_compiled';
 import type {Message} from '../types';
 
@@ -17,11 +17,9 @@ class WorkerFrontend {
 
   constructor ( methods: string, name: string, listener: Function ) {
 
-    const Worker = globalThis.Worker || WorkerShim; // Ensuring Electron is supported
+    const code = `data:text/javascript;charset=utf-8,${encodeURIComponent ( WorkerBackend.replace ( '/*! METHODS_PLACEHOLDER !*/', `\n\n\n${methods}` ) )}`;
 
-    const code = `data:text/javascript;charset=utf-8,${encodeURIComponent ( WorkerBackend.replace ( '/*! METHODS_PLACEHOLDER !*/', `\n\n\n${methods}` ) )}`; //TODO: Use Blobs instead, maybe
-
-    this.worker = new Worker ( code, { name, type: 'module' } );
+    this.worker = new WorkerShim ( code, { name, type: 'module' } );
 
     this.listen ( listener );
 
